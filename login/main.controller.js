2246acf01f66fbee97224feac84a094e70ba71c1
euricopsapalo <37977441+euricopsapalo@users.noreply.github.com>
2018-05-05 22:04:59 +0100

    Add files via upload

---
+++ login/main.controller.js
@@ -0,0 +1,60 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('login.MainController', Controller);
+
+    function Controller($scope,$window, $rootScope, $location,$route,AuthenticationService,$cookieStore) {
+        var vm = this;
+
+        if($cookieStore.get('globals')){
+            var value=$cookieStore.get('globals');
+             vm.dataLogin=value.currentUser;
+             $rootScope.dataLogin=value.currentUser;
+
+        }else{
+            vm.dataLogin=-1;
+            $rootScope.dataLogin=-1;
+        }
+        vm.login = login;
+        $rootScope.logout=logout;
+
+        function login() {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    
+                    $location.path('/');
+                    $window.location.reload();
+                   
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };  
+
+
+        function logout(){
+
+            console.log($location.path());
+             console.log($rootScope.globals.currentUser);
+            //$rootScope.$on('$locationChangeStart', function (event, next, current) {          
+            if ( $rootScope.globals.currentUser!=null) {
+                console.log('aqui');
+                AuthenticationService.ClearCredentials();
+                $location.path('/login');
+                $route.reload();
+            }
+        //});
+        }
+
+
+
+        } 
+       
+
+
+})();
